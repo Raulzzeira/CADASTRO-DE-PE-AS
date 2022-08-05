63358f8995ceb4c1be9846ee91b6058d2dc14723
Raul Almeida <109997972+Raulzzeira@users.noreply.github.com>
2022-08-05 16:46:30 -0300

    upload projeto contagem de peças

---
+++ CADASTRODEPECAS/sistema de peças.js
@@ -0,0 +1,31 @@
+let peso = 32
+
+if (peso>100) {
+    console.log('pode cadastrar')
+} else {
+    console.log('peso inferior a 100 gramas')  
+}
+
+console.log ('----------------------------------------------------')
+
+let listaPecas = 9
+
+if (listaPecas>10) {
+    console.log('limite da caixa foi ultrapassado')
+} else {
+    console.log('pode cadastrar, capacidade ok')
+}
+
+console.log('-------------------------------------------------------')
+
+var nomePeca2 = 'Raul'
+
+if (nomePeca2.length >3) {
+    console.log('pode cadastrar')
+
+} else {
+    console.log ('nome curto, possível erro')
+    
+}
+
+console.log('------------------------------------------------------');
